5d3b6a9c2231e05efc9831b91ec8cd0ceadd2e2d
jiwoo <zbqmgldjfh@gmail.com>
2021-11-22 19:24:17 +0900

    feat(SessionManager): Session 관리 class 구현
    세션을 생성하고, 조회, 만료시키는 기능을 제공

---
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -0,0 +1,49 @@
+package hello.login.web.login;
+
+import hello.login.domain.login.LoginService;
+import hello.login.domain.member.Member;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.server.Session;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class LoginController {
+
+    private final LoginService loginService;
+
+    @GetMapping("/login")
+    public String loginForm(@ModelAttribute LoginForm form){
+        return "login/loginForm";
+    }
+
+    @PostMapping("/login")
+    public String login(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response){
+        if(bindingResult.hasErrors()){
+            return "login/loginForm";
+        }
+
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+
+        if(loginMember == null){
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+        // 쿠키에 시간 정보를 주지 않으면 세션 쿠기
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
+
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/hello/login/web/session/SessionManager.java
@@ -0,0 +1,55 @@
+package hello.login.web.session;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class SessionManager {
+
+    public static final String SESSION_COOKIE_NAME = "mySessionId";
+    private Map<String, Object> sessionStroe = new ConcurrentHashMap<>();
+
+    // 세션 생성
+    public void createSession(Object value, HttpServletResponse response){
+
+        // 세션 ID 생성하고, 값을 세션에 저장
+        String sessionId = UUID.randomUUID().toString();
+        sessionStroe.put(sessionId, value);
+
+        // 쿠키 생성
+        Cookie mySessionCookie = new Cookie(SESSION_COOKIE_NAME, sessionId);
+        response.addCookie(mySessionCookie);
+    }
+
+    // 세션 조회
+    public Object getSession(HttpServletRequest request) {
+        Cookie sessionCookie = findCookie(request, SESSION_COOKIE_NAME);
+        if(sessionCookie == null){
+            return null;
+        }
+        return  sessionStroe.get(sessionCookie.getValue());
+    }
+
+    // 세션 만료
+    public void expire(HttpServletRequest request){
+        Cookie sessionCookie = findCookie(request, SESSION_COOKIE_NAME);
+        if(sessionCookie != null){
+            sessionStroe.remove(sessionCookie.getValue());
+        }
+    }
+
+    public Cookie findCookie(HttpServletRequest request, String cookieName){
+    if(request.getCookies() == null){
+        return null;
+    }
+    return Arrays.stream(request.getCookies()).filter(cookie -> cookie.getName().equals(cookieName))
+            .findAny().orElse(null);
+    }
+}
